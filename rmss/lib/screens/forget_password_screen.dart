002ec355bc7fa5be9a2a19af22caa37e0dcf51ea
Blackthorn <resturantrms@gmail.com>
2026-05-10 12:40:28 +0300

    test 201

--- rmss/lib/screens/forget_password_screen.dart
+++ rmss/lib/screens/forget_password_screen.dart
@@ -6,12 +6,8 @@ class ForgotPasswordScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Forgot Password"),
-      ),
-      body: const Center(
-        child: Text("Forgot Password Screen Placeholder"),
-      ),
+      appBar: AppBar(title: const Text("Forgot Password")),
+      body: const Center(child: Text("Forgot Password Screen Placeholder")),
     );
   }
 }
